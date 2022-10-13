b5ce134eac1e50c61cf4938719ca45d7eea91931
xavihigg <xjhiggins@gmail.com>
2022-10-12 19:13:46 -0500

    finished work

--- CNF.java
+++ CNF.java
@@ -6,8 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class CNF {
-    private ArrayList<Graph> graphs;
-    ArrayList<ArrayList<Integer>> neighbors;
+    //ArrayList<ArrayList<Integer>> neighbors;
     int cnfCount = 1;
 
     public static void main(String[] args) {
@@ -23,7 +22,7 @@ public class CNF {
     }
 
     /**
-     * Reads in a file of graphs
+     * Reads in a file of cnfs and calculates the solution based off of a vertex cover made using the cook reduction theorem.
      * 
      * @param inputFileName Filename of the text document
      * @return Boolean reflecting if could be opened and fileFormatted correctly
@@ -34,7 +33,10 @@ public class CNF {
         try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line;
             while ((line = reader.readLine()) != null) {
+                long startTime = System.nanoTime();
                 line = line.trim();
+
+                // This section reads in the cnf of the line and puts the numbers into an array.
                 String[] cnf = line.split("\\s");
                 int[] cnf3 = new int[cnf.length];
                 
@@ -46,9 +48,12 @@ public class CNF {
                         numberOfVariables = num;
                     }
                 }
-                // Create my list of descriptive nodes
+
+                // Create my list of descriptive nodes that hold the value and whether the node is in the bar or clause gadget
                 ArrayList<Node> nodes = new ArrayList<Node>();
                 int numVertices = 2*numberOfVariables + cnf3.length;
+
+                // Initialize a Graph that is an adjacency matrix to represent the cnf as a graph using both bar and clause gadgets
                 Graph graph = new Graph(numVertices);
 
                 //Adding gadgets
@@ -68,7 +73,7 @@ public class CNF {
                     
                 }
 
-                // Filling the clauses of the node array
+                // Filling the cnf values that will be the clauses into my node array
                 for (int j = 0; j < cnf3.length; j++) {
                     boolean sign;
                     if (cnf3[j] < 0) {
@@ -80,13 +85,11 @@ public class CNF {
                     }
                     
                 }
-                // for (int i =0; i < nodes.size(); i++) {
-                //     System.out.println(nodes.get(i).name);
-                // }
                 
-                //Adding clauses
+                //Adding clauses to the graph as well as connecing each vertex to itself
                 int count = 1;
                 for(int row = 2*numberOfVariables; row < numVertices; row++) {
+                    graph.addEdge(row, row);
                     if(count == 1) {
                         graph.addEdge(row, row+1);
                         graph.addEdge(row, row+2);
@@ -103,7 +106,7 @@ public class CNF {
                     }
                 }
                 
-                //Add the edges between the verticies and the gadgets
+                //Add the edges between the verticies and the gadgets in the graph
                 for(int row = 0; row < numVertices; row++) {
                     for(int column = 0; column < numVertices;column++) {
                         if(nodes.get(row).name.equals(nodes.get(column).name) && nodes.get(row).clause != nodes.get(column).clause) {
@@ -112,63 +115,26 @@ public class CNF {
                     }
                 }
 
-                // Fill all the graphs to test
-                // Graph[] testGraphs = new Graph[(int) Math.pow(2, numberOfVariables)];
-                // for(int i = 0; i < (int) Math.pow(2, numberOfVariables); i++) {
-                //     Graph temp = new Graph(numVertices);
-                //     for(int x = 0; x < numVertices; x++) {
-                //         for(int y = 0; y < numVertices; y++) {
-                //             temp.getAdjMatrix()[x][y] = graph.getAdjMatrix()[x][y];
-                //         }
-                //     }
-                //     testGraphs[i] = temp;
-                // }
-
-                // int pair = 1;
-                // for(int i = 0; i < (int) Math.pow(2, numberOfVariables); i++) {
-                //     if(i % 2 == 0) {
-                //         Graph temp = new Graph(numVertices);
-                //         temp = testGraphs[i];
-                //         if (pair == 1) {
-                //             for (int column = 2*numberOfVariables; column < numVertices; column++) {  
-                //                 temp.removeEdge(i+1, column);
-                //                 temp.removeEdge(column, i+1);
-                //                 temp.removeEdge(i+3, column);
-                //                 temp.removeEdge(column, i+3);
-                //             }
-                //             testGraphs[i] = temp;
-                //             pair++;
-                //         }
-                //     } else {
-                //         Graph temp = new Graph(numVertices);
-                //         temp = testGraphs[i];
-                //         for (int column = 2*numberOfVariables; column < numVertices; column++) {  
-                //             temp.removeEdge(i-1, column);
-                //             temp.removeEdge(column, i-1);
-                //         }
-                //         testGraphs[i] = temp;
-                //     }
-                // }
-
-                // for (int i = 0; i < testGraphs.length; i++) {
-                //     System.out.println(testGraphs[i].toString());
-                // }
-
-                //System.out.println(graph.toString());
+                // This is the k value that represent the number of nodes in the vertex cover we need to find to solve the vertex cover
                 int k = this.getK(cnf3.length / 3, numberOfVariables);
+
+                // Generate the graphs adjency list from the matrix
                 graph.generateAdjList();
-                VertexCover cover = new VertexCover(graph);
+
+                // Create the findCover object to pass the graph to be solved in the vertex cover solver.
                 findVCover findCover = new findVCover();
-                findCover.findKvertexCover(cover, k);
-                //printVertexCover(findCover.getMinCover());
+                ArrayList<Integer> results = findCover.findKvertexCover(graph, k);
+                
+                long endTime = System.nanoTime();
 
                 // Create the correct ouput format
-                if(findCover.getMinCover().size() > 0) {
+                String output = "(";
+                if(results.size() == k) {
                      System.out.print("3CNF No. " + cnfCount + ": [n="
-                    + numberOfVariables +" k=" + cnf3.length / 3 + "] (0 ms) Solution:["
+                    + numberOfVariables +" k=" + cnf3.length / 3 + "] ("+ (endTime-startTime)/1000000 + " ms) Solution:["
                     );
                     for(int l = 0; l < numberOfVariables; l++) {
-                        int value = nodes.get(findCover.getMinCover().get(l)).value;
+                        int value = nodes.get(results.get(l)).value;
                         if (value > 0) {
                             System.out.print(value + ":T ");
                         } else {
@@ -176,13 +142,111 @@ public class CNF {
                         }
                     }
                     System.out.println("]");
-                } else {
-                    System.out.println("NO SOLUTION");
-                }
-                cnfCount++;
-                
+                    for(int i = 0; i < cnf3.length; i++) {
+                        if(i == 0) {
+                            if (cnf3[i] > 0){
+                                output += " " + cnf3[i] + "|";
+                            } else {
+                                output += cnf3[i] + "|";
+                            }
+                        } else if((i+1) % 3 != 0 && i != cnf3.length-1) {
+                            if(cnf3[i] > 0){
+                                output += " " + cnf3[i] + "|";
+                            } else {
+                                output += cnf3[i] + "|";
+                            }
+                            
+                        } else if(i != cnf3.length-1) {
+                            if(cnf3[i] > 0){
+                                output += " "+ cnf3[i] + ")^(";
+                            } else {
+                                output += cnf3[i] +")^(";
+                            }
+                            
+                        } else {
+                            if(cnf3[i] > 0){
+                                output += " " + cnf3[i] + ")";
+                            } else {
+                                output += cnf3[i] + ")";
+                            }
+                        }
+                    }
 
+                    System.out.println(output + " ==>");
+                    for (int x = 0; x < numberOfVariables; x++) {
+                        int val = nodes.get(results.get(x)).value;
+                        if(val > 0) {
+                            output = output.replace(""+val, "T");
+                            output = output.replace("-T", " F");
+                        } else  {
+                            output = output.replace("" + Math.abs(val), "F");
+                            output = output.replace("-F", " T");
+                        }
+                    }
+                    System.out.println(output);
+                    System.out.println(" ");
+                } else { // for when the cover is wrong or when there is no solution
+                    int[] fakeResults = new int[numberOfVariables];
+                    for(int i = 0; i < numberOfVariables; i++) {
+                        fakeResults[i] = i+1;
+                    }
+                    System.out.print("3CNF No. " + cnfCount + ": [n="
+                    + numberOfVariables +" k=" + cnf3.length / 3 + "] ("+ (endTime-startTime)/1000000 + " ms) No Solution Random:["
+                    );
+                    for(int l = 0; l < numberOfVariables; l++) {
+                        int value = fakeResults[l];
+                        if (value > 0) {
+                            System.out.print(value + ":T ");
+                        } else {
+                            System.out.print(Math.abs(value) + ":F ");
+                        }
+                    }
+                    System.out.println("]");
+                    for(int i = 0; i < cnf3.length; i++) {
+                        if(i == 0) {
+                            if (cnf3[i] > 0){
+                                output += " " + cnf3[i] + "|";
+                            } else {
+                                output += cnf3[i] + "|";
+                            }
+                        } else if((i+1) % 3 != 0 && i != cnf3.length-1) {
+                            if(cnf3[i] > 0){
+                                output += " " + cnf3[i] + "|";
+                            } else {
+                                output += cnf3[i] + "|";
+                            }
+                            
+                        } else if(i != cnf3.length-1) {
+                            if(cnf3[i] > 0){
+                                output += " "+ cnf3[i] + ")^(";
+                            } else {
+                                output += cnf3[i] +")^(";
+                            }
+                            
+                        } else {
+                            if(cnf3[i] > 0){
+                                output += " " + cnf3[i] + ")";
+                            } else {
+                                output += cnf3[i] + ")";
+                            }
+                        }
+                    }
+                    System.out.println(output + " ==>");
+                    for (int x = 0; x < numberOfVariables; x++) {
+                        int val = fakeResults[x];
+                        if(val > 0) {
+                            output = output.replace(""+val, "T");
+                            output = output.replace("-T", " F");
+                        } else  {
+                            output = output.replace("" + val, "F");
+                            output = output.replace("-F", " T");
+                        }
+                    }
+                    System.out.println(output);
+                    System.out.println(" ");
+                }
                 
+                cnfCount++; // increase the number of lines read by one
             }
 
         } catch (IOException e) {
@@ -194,16 +258,5 @@ public class CNF {
 
     public int getK(int numberOfClauses, int numberOfVariables) {
         return numberOfVariables + (2 * numberOfClauses);
-    }
-
-    private void printVertexCover(ArrayList<Integer> vertexCover) {
-        System.out.print("{");
-        for (int j = 0; j < vertexCover.size() - 1; j++) {
-            System.out.print(vertexCover.get(j) + ", ");
-        }
-        if (vertexCover.size() != 0) {
-            System.out.print(vertexCover.get(vertexCover.size() - 1));
-        }
-        System.out.print("}\n");
     }
 }
